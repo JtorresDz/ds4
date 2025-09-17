18eff45d50ba8b1cb5b37c3b3eab37de79c917bb
Jose Torres <joseant365@gmail.com>
2025-09-16 19:15:17 -0500

    Agregado el Laboratorio2

---
+++ Laboratorio2/Laboratorio21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Client client = new Client();
+
+        client.FirstName = "Su Nombre";
+        client.LastName = "Su Apellido";
+        client.Age = 30;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+        }
+
+    }
+    
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+}
+
+
+
+
