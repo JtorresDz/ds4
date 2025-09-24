dc422505e980eeb14d31fd88d2bc2292fa7f6e05
Jose Torres <joseant365@gmail.com>
2025-09-23 20:12:45 -0500

    Laboratorio 6 completo

---
+++ Laboratorio6/Laboratorio6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.Write("Digite el numero deseado: ");
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("No ha introducido un digito valido");
+            num = -1;
+        }
+        Console.WriteLine(num);
+    }
+}
---
+++ Laboratorio6/Laboratorio64/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acceso Negado - No cumple con el criterio de edad.");
+        }
+        else
+        {
+            Console.WriteLine("Acceso concedido.");
+        }
+    }
+    static void Main(string[] args)
+    {
+       checkAge(15);
+    }
+}
