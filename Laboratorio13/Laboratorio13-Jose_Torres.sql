dc4c3aa87dccab3999e0aea90b18ada6203e63a8
Jose Torres <joseant365@gmail.com>
2025-10-16 20:41:34 -0500

    Laboratorio13 completo

---
+++ Laboratorio13/Laboratorio13-Jose_Torres.sql
@@ -0,0 +1,4 @@
+SELECT ProductName, CompanyName, ContactName
+FROM Products P 
+FULL JOIN Suppliers S
+ON P.SupplierID=S.SupplierID	
---
+++ Laboratorio13/Laboratorio13/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Laboratorio13
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=.\sqlexpress;Database=Northwind;TrustServerCertificate=true;Integrated Security=SSPI;";
+
+        string query = "SELECT ProductName FROM [dbo].[Products]";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            MessageBox.Show("Conexión Exitosa");
+            connection.Close();
+            MessageBox.Show("Conexión Cerrada");
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["ProductName"].ToString());
+            }
+            connection.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
